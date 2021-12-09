f2f6c78f4f5d3187d4ddef4f39fd47e94597bbb4
kazushi.hirai <policeman_kh@msn.com>
2021-12-09 22:23:41 +0900

    Update

--- src/main/java/sandbox/armeria_k8s/ArmeriaK8sApplication.java
+++ src/main/java/sandbox/armeria_k8s/ArmeriaK8sApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ArmeriaK8sApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ArmeriaK8sApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ArmeriaK8sApplication.class, args);
+    }
 
 }
